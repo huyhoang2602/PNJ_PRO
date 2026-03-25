dfcd29e5dcea4911947e9f89749b7ee7639600ae
huyhoang <huyhoangtran2602@gmail.com>
2026-03-25 17:16:54 +0700

    udpate:bộ sưu tập

---
+++ admin/language/en-gb/module/dc_collection.php
@@ -0,0 +1,23 @@
+<?php
+// Heading
+$_['heading_title']    = 'DC Minimal - Collections';
+
+// Text
+$_['text_extension']   = 'Extensions';
+$_['text_success']     = 'Success: You have modified DC Minimal - Collections module!';
+$_['text_edit']        = 'Edit DC Minimal - Collections Module';
+
+// Entry
+$_['entry_name']       = 'Module Name';
+$_['entry_title']      = 'Main Title';
+$_['entry_status']     = 'Status';
+$_['entry_collection_title'] = 'Collection Name';
+$_['entry_image']      = 'Banner Image';
+$_['entry_product']    = 'Products';
+
+// Help
+$_['help_product']     = '(Autocomplete)';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify DC Minimal - Collections module!';
+$_['error_name']       = 'Module Name must be between 3 and 64 characters!';
---
+++ catalog/controller/module/dc_collection.php
@@ -0,0 +1,79 @@
+<?php
+namespace Opencart\Catalog\Controller\Extension\DcMinimal\Module;
+
+class DcCollection extends \Opencart\System\Engine\Controller {
+	public function index(array $setting): string {
+		$this->load->language('extension/dc_minimal/module/dc_collection');
+
+		$this->load->model('catalog/product');
+		$this->load->model('tool/image');
+
+		$data['title'] = $setting['title'] ?? 'Bộ sưu tập mới';
+        $data['uid'] = rand(1000, 99999);
+        $data['collections'] = [];
+
+        if (!empty($setting['collections'])) {
+            foreach ($setting['collections'] as $key => $collection) {
+                $image = '';
+                if (is_file(DIR_IMAGE . html_entity_decode($collection['image'], ENT_QUOTES, 'UTF-8'))) {
+                    $image = HTTP_SERVER . 'image/' . html_entity_decode($collection['image'], ENT_QUOTES, 'UTF-8');
+                }
+
+                $products = [];
+                if (!empty($collection['product'])) {
+                    foreach ($collection['product'] as $product_id) {
+                        $product_info = $this->model_catalog_product->getProduct($product_id);
+
+                        if ($product_info) {
+                            if (is_file(DIR_IMAGE . html_entity_decode($product_info['image'], ENT_QUOTES, 'UTF-8'))) {
+                                $product_image = $this->model_tool_image->resize(html_entity_decode($product_info['image'], ENT_QUOTES, 'UTF-8'), 300, 300);
+                            } else {
+                                $product_image = $this->model_tool_image->resize('placeholder.png', 300, 300);
+                            }
+
+                            if ($this->customer->isLogged() || !$this->config->get('config_customer_price')) {
+                                $price = $this->currency->format($this->tax->calculate($product_info['price'], $product_info['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
+                            } else {
+                                $price = false;
+                            }
+
+                            if ((float)$product_info['special']) {
+                                $special = $this->currency->format($this->tax->calculate($product_info['special'], $product_info['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
+                            } else {
+                                $special = false;
+                            }
+
+                            $products[] = [
+                                'product_id'  => $product_info['product_id'],
+                                'thumb'       => $product_image,
+                                'name'        => $product_info['name'],
+                                'description' => oc_substr(trim(strip_tags(html_entity_decode($product_info['description'], ENT_QUOTES, 'UTF-8'))), 0, $this->config->get('config_product_description_length')) . '..',
+                                'price'       => $price,
+                                'special'     => $special,
+                                'tax'         => ($this->config->get('config_tax') ? $this->currency->format((float)$product_info['special'] ? $product_info['special'] : $product_info['price'], $this->session->data['currency']) : false),
+                                'href'        => $this->url->link('product/product', 'product_id=' . $product_info['product_id'])
+                            ];
+                        }
+                    }
+                }
+
+                $lang_code = $this->config->get('config_language');
+                $collection_title = $collection['title'][$lang_code] ?? ($collection['title']['en-gb'] ?? 'Collection');
+
+                $data['collections'][] = [
+                    'id'       => $key,
+                    'title'    => $collection_title,
+                    'image'    => $image,
+                    'link'     => $collection['link'] ?? '',
+                    'products' => $products
+                ];
+            }
+        }
+
+        if (empty($data['collections'])) {
+            return '';
+        }
+
+		return $this->load->view('extension/dc_minimal/module/dc_collection', $data);
+	}
+}
